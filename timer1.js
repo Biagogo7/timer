4944d837d4db9bb82860ac97ee5e495adb4b89f4
Adeyemi <henry.atayero@gmail.com>
2022-02-09 18:37:56 -0600

    adding a timer

---
+++ timer1.js
@@ -0,0 +1,21 @@
+
+const timer1 = function() {
+
+  const timer = process.argv;
+
+  const xtimer = timer.splice(2)
+  const sortedTimer = xtimer.sort()  
+  
+  sortedTimer.forEach(time => {
+    setTimeout(() => { 
+      console.log('timer', time)
+      process.stdout.write('\x07');
+      console.log('sound')
+    }, time * 1000 )
+  })      
+  
+  
+}
+
+const trest = timer1()
+
